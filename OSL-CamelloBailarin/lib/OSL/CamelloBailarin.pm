9e0cbbd75a1b51e6796743ea89440a52f8f29e77
German Martinez <germaaan@gmail.com>
2016-10-25 10:06:03 +0200

    Pequeños cambios en HTML

--- OSL-CamelloBailarin/lib/OSL/CamelloBailarin.pm
+++ OSL-CamelloBailarin/lib/OSL/CamelloBailarin.pm
@@ -28,7 +28,6 @@ sub set_flash {
 }
 
 sub get_flash {
-
     my $msg = $flash;
     $flash = "";
 
@@ -61,20 +60,16 @@ get '/' => sub {
     my $sql = 'select id, title, text from entries order by id desc';
     my $sth = $db->prepare($sql) or die $db->errstr;
     $sth->execute or die $sth->errstr;
-    template 'show_entries.tt', {
+    template 'data.tt', {
         'msg' => get_flash(),
         'add_entry_url' => uri_for('/add'),
         'entries' => $sth->fetchall_hashref('id'),
     };
 };
 
-# get '/' => sub {
-#     template 'index';
-# };
-
 post '/add' => sub {
     if ( not session('logged_in') ) {
-        send_error("Not logged in", 401);
+        send_error("Sin iniciar sesión.", 401);
     }
 
     my $db = connect_db();
@@ -82,7 +77,7 @@ post '/add' => sub {
     my $sth = $db->prepare($sql) or die $db->errstr;
     $sth->execute(params->{'title'}, params->{'text'}) or die $sth->errstr;
 
-    set_flash('New entry posted!');
+    set_flash('Nueva entrada añadida.');
     redirect '/';
 };
 
@@ -90,30 +85,27 @@ any ['get', 'post'] => '/login' => sub {
     my $err;
 
     if ( request->method() eq "POST" ) {
-        # process form input
         if ( params->{'username'} ne setting('username') ) {
-            $err = "Invalid username";
+            $err = "Usuario no válido.";
         }
         elsif ( params->{'password'} ne setting('password') ) {
-            $err = "Invalid password";
+            $err = "Password no válido.";
         }
         else {
             session 'logged_in' => true;
-            set_flash('You are logged in.');
+            set_flash('Sesión iniciada.');
             return redirect '/';
         }
    }
 
-   # display login form
    template 'login.tt', {
        'err' => $err,
    };
-
 };
 
 get '/logout' => sub {
    app->destroy_session;
-   set_flash('You are logged out.');
+   set_flash('Sesión finalizada.');
    redirect '/';
 };
 

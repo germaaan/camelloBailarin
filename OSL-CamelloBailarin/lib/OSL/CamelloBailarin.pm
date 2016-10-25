e29003c5e0e2f1225db755940396c231b0f17989
German Martinez <germaaan@gmail.com>
2016-10-25 06:38:02 +0200

    Añade conexión base de datos

--- OSL-CamelloBailarin/lib/OSL/CamelloBailarin.pm
+++ OSL-CamelloBailarin/lib/OSL/CamelloBailarin.pm
@@ -1,7 +1,21 @@
 package OSL::CamelloBailarin;
 use Dancer2;
+use DBI;
 
 our $VERSION = '0.1';
+
+sub connect_db {
+  my $dbh = DBI->connect("dbi:SQLite:dbname=".setting('database')) or
+     die $DBI::errstr;
+
+  return $dbh;
+}
+
+sub init_db {
+  my $db = connect_db();
+  my $schema = read_text('./schema.sql');
+  $db->do($schema) or die $db->errstr;
+}
 
 get '/' => sub {
     template 'index';

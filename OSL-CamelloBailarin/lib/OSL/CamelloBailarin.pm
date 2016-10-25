e48cc78d3789b90b012389c3c303a717db6dc0b6
German Martinez <germaaan@gmail.com>
2016-10-25 06:44:57 +0200

    Añade ruta raiz

--- OSL-CamelloBailarin/lib/OSL/CamelloBailarin.pm
+++ OSL-CamelloBailarin/lib/OSL/CamelloBailarin.pm
@@ -1,8 +1,39 @@
 package OSL::CamelloBailarin;
 use Dancer2;
 use DBI;
+use File::Spec;
+use File::Slurper qw/ read_text /;
+use Template;
+
+set 'database'     => File::Spec->catfile(File::Spec->tmpdir(), 'dancr.db');
+set 'session'      => 'Simple';
+set 'template'     => 'template_toolkit';
+set 'logger'       => 'console';
+set 'log'          => 'debug';
+set 'show_errors'  => 1;
+set 'startup_info' => 1;
+set 'warnings'     => 1;
+set 'username'     => 'admin';
+set 'password'     => 'password';
+set 'layout'       => 'main';
 
 our $VERSION = '0.1';
+
+my $flash;
+
+sub set_flash {
+    my $message = shift;
+
+    $flash = $message;
+}
+
+sub get_flash {
+
+    my $msg = $flash;
+    $flash = "";
+
+    return $msg;
+}
 
 sub connect_db {
   my $dbh = DBI->connect("dbi:SQLite:dbname=".setting('database')) or
@@ -18,7 +49,19 @@ sub init_db {
 }
 
 get '/' => sub {
-    template 'index';
+    my $db = connect_db();
+    my $sql = 'select id, title, text from entries order by id desc';
+    my $sth = $db->prepare($sql) or die $db->errstr;
+    $sth->execute or die $sth->errstr;
+    template 'show_entries.tt', {
+        'msg' => get_flash(),
+        'add_entry_url' => uri_for('/add'),
+        'entries' => $sth->fetchall_hashref('id'),
+    };
 };
+
+# get '/' => sub {
+#     template 'index';
+# };
 
 true;
